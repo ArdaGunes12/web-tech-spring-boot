2df7faf26dcc1eabd10aaccb98726fe2a2b4a2e5
Steven Lehmann <S.Lehmann@Student.HTW-Berlin.de>
2025-05-25 17:31:55 +0200

    MessageController in anderes Verzeichnis verschoben

--- src/main/java/de/htwberlin/webtech/web/MessageController.java
+++ src/main/java/de/htwberlin/webtech/web/MessageController.java
@@ -1,4 +1,4 @@
-package de.htwberlin.webtech;
+package de.htwberlin.webtech.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
